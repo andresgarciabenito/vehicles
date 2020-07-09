435c9fb9ae1aa9c94bdbdc741b79b1490ede7a56
andresgarciabenito <andresgarciabenito@hotmail.com>
2020-07-09 13:39:13 +0200

    corregido

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,46 +1,62 @@
+var car;
+var form = document.getElementById("main_form");
 //Capturar el submit
-document.getElementById('car-form').addEventListener('submit', function () {
-    var plate = document.getElementById('plate').value;
-    var brand = document.getElementById('brand').value;
-    var color = document.getElementById('color').value;
-    //console.log (plate, brand, color);
-    //creo un nuevo objeto con la estructura de la clase Car
-    var car = new Car(plate, brand, color, wheels);
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    var errors = 0;
+    var plate = document.getElementById("plate").value; // contingut inputs cotxe
+    var brand = document.getElementById("brand").value;
+    var color = document.getElementById("color").value;
+    console.log(plate, brand, color);
     //nueva instancia de la interfaz
     var ui = new UI();
     //función de validación de patente
-    function validatePlate(e) {
-        var regex = /[0-9]{4}[A-Z]{3}/g;
-        return regex.test(e) ? true : false;
+    if (plate === "") {
+        errors++;
+    }
+    if (brand === "") {
+        errors++;
+    }
+    if (color === "") {
+        errors++;
     }
     //Validaciones primer formulario (car)
-    if (plate === "" || brand === "" || color === "") {
-        alert("Fill the fields"); //Reemplazar el alert con showMessage   
-        return; //no permite que se continúe con el código. 
+    if (errors > 0) {
+        alert("Fill the fields"); //Reemplazar el alert con showMessage
+        return; //no permite que se continúe con el código.
     }
-    else if (!validatePlate(plate)) {
+    if (!validatePlate(plate)) {
         alert("Insert plate, format 1234ABC"); //Reemplazar el alert con showMessage
-        return;
+        return; //no permite que se continúe con el código.
     }
+    //creo un nuevo objeto con la estructura de la clase Car
+    var car = new Car(plate, brand, color);
     ui.addCar(car);
     ui.restoreForm();
     return;
 });
-//Capturar el submit del segundo form (wheels)
-document.getElementById('wheel-form').addEventListener('submit', function () {
+function validatePlate(e) {
+    var regex = /[0-9]{4}[A-Z]{3}/g;
+    return regex.test(e) ? true : false;
+}
+var wheel_form = document.getElementById("wheel-form");
+//Capturar el submit
+wheel_form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
     //Validaciones segundo formulario (wheels)
     for (var i = 1; i <= 4; i++) {
-        var brand = document.getElementById("brand" + i);
-        var diam = document.getElementById("diam" + i);
-        if (diam.value < 0.4 || diam.value > 2) {
-            alert("Incorrect diameter (wheel "(i) + ")");
+        var brand = document.getElementById("brand" + i)
+            .value;
+        var diam = Number(document.getElementById("diam" + i).value);
+        if (diam < 0.4 || diam > 2) {
+            alert("Incorrect diameter");
             break;
         }
         else
-            var wheels = new Wheel(brand, diam);
+            var wheels = new Wheel(diam, brand);
     }
     var ui = new UI();
-    ui.addWheels(wheels);
-    //evitar que se resetee el formulario(acción por defecto)
-    event.preventDefault();
+    car.addWheel(wheels);
 });
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,59 +1,83 @@
+var car: Car;
 
+var form = document.getElementById("main_form") as HTMLFormElement;
 
 //Capturar el submit
-document.getElementById('car-form').addEventListener('submit', function(){
-    const plate=document.getElementById('plate').value;
-    const brand=document.getElementById('brand').value;
-    const color=document.getElementById('color').value;
-    //console.log (plate, brand, color);
-    
-    //creo un nuevo objeto con la estructura de la clase Car
-    const car= new Car (plate, brand, color, wheels);
-    //nueva instancia de la interfaz
-    const ui= new UI();
-    
-    //función de validación de patente
-    function validatePlate (e){
-        var regex=/[0-9]{4}[A-Z]{3}/g;
-        return regex.test(e)?true:false;
-    }
-    
-    //Validaciones primer formulario (car)
-    
-    if (plate===""||brand===""||color===""){
-      alert("Fill the fields");                                         //Reemplazar el alert con showMessage   
-      return; //no permite que se continúe con el código. 
-    }else if(!validatePlate(plate)){
-      alert("Insert plate, format 1234ABC");                            //Reemplazar el alert con showMessage
-      return;
-    }
-    
-    ui.addCar(car);
-    
-    ui.restoreForm();
-    return;
-    });
-    
-    
-//Capturar el submit del segundo form (wheels)
-document.getElementById('wheel-form').addEventListener('submit', function(){
-       
-    //Validaciones segundo formulario (wheels)
-    for (let i = 1; i <= 4; i++) {
-        let brand=<HTMLElement>document.getElementById("brand" + i);
-        let diam=<HTMLElement>document.getElementById("diam" + i);
-        
-        if (diam.value<0.4||diam.value>2){
-        alert("Incorrect diameter (wheel "(i)+")");                                     
-        break;  
-      }else 
-    let wheels= new Wheel (brand, diam);
-    }
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  event.stopPropagation();
 
-    const ui= new UI();
-    
-    ui.addWheels(wheels);
-    //evitar que se resetee el formulario(acción por defecto)
-    event.preventDefault();
-    
-    });
+  var errors = 0;
+
+  const plate = (document.getElementById("plate") as HTMLInputElement).value; // contingut inputs cotxe
+  const brand = (document.getElementById("brand") as HTMLInputElement).value;
+  const color = (document.getElementById("color") as HTMLInputElement).value;
+  console.log (plate, brand, color);
+
+  //nueva instancia de la interfaz
+  const ui = new UI();
+
+  //función de validación de patente
+
+  if (plate === "") {
+    errors++;
+  }
+
+  if (brand === "") {
+    errors++;
+  }
+
+  if (color === "") {
+    errors++;
+  }
+
+  //Validaciones primer formulario (car)
+
+  if (errors > 0) {
+    alert("Fill the fields"); //Reemplazar el alert con showMessage
+    return; //no permite que se continúe con el código.
+  }
+
+  if (!validatePlate(plate)){
+    alert("Insert plate, format 1234ABC");                            //Reemplazar el alert con showMessage
+    return; //no permite que se continúe con el código.
+  }
+  //creo un nuevo objeto con la estructura de la clase Car
+  const car = new Car(plate, brand, color);
+
+  ui.addCar(car);
+
+  ui.restoreForm();
+  return;
+});
+
+function validatePlate(e: any) {
+  var regex = /[0-9]{4}[A-Z]{3}/g;
+  return regex.test(e) ? true : false;
+}
+
+var wheel_form = document.getElementById("wheel-form") as HTMLFormElement;
+
+//Capturar el submit
+wheel_form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  //Validaciones segundo formulario (wheels)
+  for (let i = 1; i <= 4; i++) {
+    let brand = (document.getElementById("brand" + i) as HTMLInputElement)
+      .value;
+    let diam = Number(
+      (document.getElementById("diam" + i) as HTMLInputElement).value
+    );
+
+    if (diam < 0.4 || diam > 2) {
+      alert ("Incorrect diameter");
+      break;
+    } else var wheels = new Wheel(diam, brand);
+  }
+
+  const ui = new UI();
+
+  car.addWheel(wheels);
+});
